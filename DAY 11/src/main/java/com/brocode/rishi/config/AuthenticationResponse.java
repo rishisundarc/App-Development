0a8cf6ed98a918a112ad04818d82a3a06e62cde4
RISHI SUNDAR C <119105402+rishisundarc@users.noreply.github.com>
2023-10-05 10:36:15 +0530

    Add files via upload

---
+++ DAY 11/src/main/java/com/brocode/rishi/config/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.brocode.rishi.config;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+private String token;
+}
